ed81ae3adfa0648b6a807f80b711afffb74b2366
MishaLybin <116936366+MishaLybin@users.noreply.github.com>
2023-04-09 19:52:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,97 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+int priority(char x) {
+    switch (x) {
+        case '(':
+            return 0;
+        case ')':
+            return 1;
+        case '+':
+            return 2;
+        case '-':
+            return 2;
+        case '*':
+            return 3;
+        case '/':
+            return 3;
+        default:
+            return -1;
+    }
 }
 
-int eval(std::string pref) {
-  // добавьте код
-  return 0;
+std::string infx2pstfx(std::string fin) {
+  TStack<char, 100> stack1;
+    std::string n;
+    for (int i = 0; i < fin.size(); ++i) {
+        if (PriorityOfOperator(fin[i]) == 0) {
+            stack1.push(fin[i]);
+        } else if (PriorityOfOperator(fin[i]) > stack1.get()
+                   && !stack1.isEmpty()) {
+            stack1.push(fin[i]);
+        } else if (stack1.isEmpty() && PriorityOfOperator(fin[i]) != -2) {
+            stack1.push(fin[i]);
+        } else {
+            if (PriorityOfOperator(fin[i]) == 1) {
+                while (PriorityOfOperator(stack1.get()) != 0) {
+                    n += stack1.pop();
+                    n += ' ';
+                }
+                stack1.pop();
+            } else if (PriorityOfOperator(inf[i]) == -2
+                       && i != fin.size() - 1) {
+                int j;
+                for (j = i; PriorityOfOperator(fin[j]) == -2; ++j) {
+                    n += fin[j];
+                }
+                i += j - i - 1;
+                n += ' ';
+            } else {
+                while (PriorityOfOperator(stack1.get()) >=
+                       PriorityOfOperator(fin[i])
+                       && PriorityOfOperator(fin[i]) != -2) {
+                    n += stack1.pop();
+                    n += ' ';
+                }
+                stack1.push(fin[i]);
+            }
+        }
+        if (i == fin.size() - 1) {
+            while (!stack1.isEmpty()) {
+                n += stack1.pop();
+                n += ' ';
+            }
+        }
+    }
+    n.pop_back();
+    return n;
+}
+
+int eval(std::string cr) {
+  TStack<int, 100> stack2;
+    for (char a : cr) {
+        if (isdigit(a)) {
+            stack2.push(a - '0');
+        } else if (a == ' ') {
+            continue;
+        } else {
+            int second = stack2.pop();
+            int first = stack2.pop();
+            switch (symb) {
+            case ('+'):
+                stack2.push(first + second);
+                break;
+            case ('-'):
+                stack2.push(first - second);
+                break;
+            case ('*'):
+                stack2.push(first * second);
+                break;
+            case ('/'):
+                stack2.push(first / second);
+                break;
+            }
+        }
+    }
+    return stack2.get();
 }
