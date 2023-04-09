744d8420111f96d83119b2520a0bc840fb4d5b75
MishaLybin <116936366+MishaLybin@users.noreply.github.com>
2023-04-09 21:40:25 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,40 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+// Copyright 2021 NNTU-CS
+#ifndef INCLUDE_TSTACK_H_
+#define INCLUDE_TSTACK_H_
+#include <string>
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T* arr;
+    int top;
+
+ public:
+    TStack(): top(-1) { arr = new T[size]; }
+    void push(T value) {
+        if (isFull())
+            throw std::string("Full");
+        else
+            arr[++top] = value;
+    }
+    const T& pop() {
+        if (isEmpty())
+            throw std::string("Empty");
+        else
+            return arr[top--];
+    }
+    const T& get() const {
+        return arr[top];
+    }
+    bool isEmpty() const {
+        return top == -1;
+    }
+    bool isFull() const {
+        return top == size - 1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
